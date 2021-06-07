bffc2d71bb359348e8db8d4aafbac226989cab7e
EC2 Default User <ec2-user@ip-172-31-4-240.ec2.internal>
2021-06-07 23:23:42 +0000

    fix script for cdk

--- chaos-01-deploy-cdk-base-stack.sh
+++ chaos-01-deploy-cdk-base-stack.sh
@@ -6,7 +6,9 @@ WORK_DIR_PATH=`cd ${SCRIPT_DIR_NAME}; pwd -P`
 
 . ${WORK_DIR_PATH}/chaos-env.sh
 
-cd ${WORK_DIR_PATH}/chaos-stack && cdk deploy ChaosBaseStack --require-approval never --outputs-file ${CDK_OUTPUT_FILE_CHAOS_BASE_STACK}
+sudo yum install -y jq
+
+cd ${WORK_DIR_PATH}/chaos-stack && npm install && cdk bootstrap && cdk deploy ChaosBaseStack --require-approval never --outputs-file ${CDK_OUTPUT_FILE_CHAOS_BASE_STACK}
 
 CHAOS_BUCKET_NAME=`cat ${CDK_OUTPUT_FILE_CHAOS_BASE_STACK} | jq -r '.ChaosBaseStack.chaosBucketName'`
 echo "CHAOS_BUCKET_NAME: ${CHAOS_BUCKET_NAME}"
